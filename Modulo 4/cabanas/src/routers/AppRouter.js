2fcb1ba6b42f5eac95f499d3dd90116db274637b
Javier Lopez <jvrelopez@gmail.com>
2021-09-28 14:16:13 -0300

    Acomodando navbar

--- Modulo 4/cabanas/src/routers/AppRouter.js
+++ Modulo 4/cabanas/src/routers/AppRouter.js
@@ -4,23 +4,27 @@ import {
     Switch,
     Route
 } from 'react-router-dom';
-import { Navbar } from '../components/layouts/Navbar';
-import { Home } from '../components/layouts/Home';
-import { Imagenes } from '../components/sections/Imagenes';
-import { Servicios } from '../components/sections/Servicios';
 
+import { Navbar } from '../components/layouts/Navbar';
+import { Home } from '../components/pages/Home';
+import { Imagenes } from '../components/pages/Imagenes';
+import { Servicios } from '../components/pages/Servicios';
+import { Precios } from '../components/pages/Precios';
+import { Contacto } from '../components/pages/Contacto';
 
 export const AppRouter = () => {
     return (
         <Router>
-            <div id="header" className = "fixed-top" >
+            <header id="header" className = "header" >
                 <Navbar />
+            </header>
                 <Switch>
                     <Route exact path="/" component={ Home } />
                     <Route exact path="/servicios" component={ Servicios } />
                     <Route exact path="/imagenes" component={ Imagenes } />
+                    <Route exact path="/precios" component={ Precios } />
+                    <Route exact path="/contacto" component={ Contacto } />
                 </Switch>
-            </div>
         </Router>
     )
 }
