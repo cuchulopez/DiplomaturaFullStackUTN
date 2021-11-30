bd7c1a5f71e5e314755ddbfa7328aede8c976cfb
Javier Lopez <jvrelopez@gmail.com>
2021-11-30 09:37:38 -0300

    Acomodando estilos

--- ProyectoFinal/backend/models/cabanas.sql
+++ ProyectoFinal/backend/models/cabanas.sql
@@ -1,10 +1,3 @@
--- --------------------------------------------------------
--- Host:                         192.168.100.21
--- Versión del servidor:         10.6.5-MariaDB-1:10.6.5+maria~focal - mariadb.org binary distribution
--- SO del servidor:              debian-linux-gnu
--- HeidiSQL Versión:             11.3.0.6295
--- --------------------------------------------------------
-
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET NAMES utf8 */;
 /*!50503 SET NAMES utf8mb4 */;
@@ -12,12 +5,9 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-
--- Volcando estructura de base de datos para cabanas
 CREATE DATABASE IF NOT EXISTS `cabanas` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
 USE `cabanas`;
 
--- Volcando estructura para tabla cabanas.imagenes
 CREATE TABLE IF NOT EXISTS `imagenes` (
   `id` tinyint(3) unsigned NOT NULL AUTO_INCREMENT,
   `descripcion` varchar(100) NOT NULL DEFAULT '0',
@@ -25,17 +15,14 @@ CREATE TABLE IF NOT EXISTS `imagenes` (
   KEY `Índice 1` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
 
--- Volcando datos para la tabla cabanas.imagenes: ~2 rows (aproximadamente)
-DELETE FROM `imagenes`;
 /*!40000 ALTER TABLE `imagenes` DISABLE KEYS */;
 INSERT INTO `imagenes` (`id`, `descripcion`, `imagen_id`) VALUES
-	(2, 'pic3', 'x54pedyaccgoree11vgl'),
-	(1, 'pic2', 'whaobtcqjalu4rvcurka'),
-	(4, 'img4', 'zth2kvcona9lftyll852'),
-	(5, 'img1', 'eeshoryfy6r0c0o202ck');
+	(2, 'Imagen 2', 'x54pedyaccgoree11vgl'),
+	(1, 'Imagen 1', 'xh9cjxn7x2nolgqz5mkg'),
+	(4, 'Imagen 3', 'zth2kvcona9lftyll852'),
+	(5, 'Imagen 4', 'eeshoryfy6r0c0o202ck');
 /*!40000 ALTER TABLE `imagenes` ENABLE KEYS */;
 
--- Volcando estructura para tabla cabanas.precios
 CREATE TABLE IF NOT EXISTS `precios` (
   `id` tinyint(3) unsigned NOT NULL AUTO_INCREMENT,
   `nro_depto` tinyint(3) unsigned DEFAULT 0,
@@ -44,12 +31,9 @@ CREATE TABLE IF NOT EXISTS `precios` (
   KEY `Índice 1` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- Volcando datos para la tabla cabanas.precios: ~0 rows (aproximadamente)
-DELETE FROM `precios`;
 /*!40000 ALTER TABLE `precios` DISABLE KEYS */;
 /*!40000 ALTER TABLE `precios` ENABLE KEYS */;
 
--- Volcando estructura para tabla cabanas.servicios
 CREATE TABLE IF NOT EXISTS `servicios` (
   `id` tinyint(3) unsigned NOT NULL AUTO_INCREMENT,
   `titulo` varchar(50) NOT NULL,
@@ -58,20 +42,17 @@ CREATE TABLE IF NOT EXISTS `servicios` (
   KEY `Índice 1` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4;
 
--- Volcando datos para la tabla cabanas.servicios: ~6 rows (aproximadamente)
-DELETE FROM `servicios`;
 /*!40000 ALTER TABLE `servicios` DISABLE KEYS */;
 INSERT INTO `servicios` (`id`, `titulo`, `descripcion`, `icono`) VALUES
 	(2, 'Segundo2', 'Segundo Expedita veritatis consequuntur nihil tempore laudantium vitae denat pactaExpedita veritatis consequuntur nihil tempore laudantium vitae denat pacta', 'fas fa-coffee'),
 	(3, 'Tercero3', 'Tercero3 Expedita veritatis consequuntur nihil tempore lauExpedita veritatis consequuntur nihil tempore laudantium vitae denat pactaExpedita veritatis consequuntur nihil tempore laudantium vitae denat pactadantium vitae denat pacta', 'fas fa-wifi'),
 	(4, 'Cuarto', 'Cuarto Expedita veritatis consequuntur nihil tempore laudantium vitae denat pactaExpedita veritatis consequuntur nihil tempore laudantium vitae denat pacta.', 'fas fa-umbrella-beach'),
-	(5, 'Bicis', 'Alquiler de bicicletas', 'fas fa-biking'),
-	(13, 'Quinto', 'QuintO', 'fab fa-android'),
-	(14, 'Cuarto', 'Cuarto 4', 'fab fa-android'),
-	(18, 'Algo3', 'Algo3', 'fas fa-question');
+	(5, 'Bicis', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro facilis aut sunt officia rem vel, labore voluptatibus! Saepe, aperiam architecto?', 'fas fa-biking'),
+	(13, 'Quinto', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea fuga voluptas, molestiae sit blanditiis ullam voluptates error amet. Neque temporibus nostrum quod rem accusantium soluta, commodi iure mollitia incidunt qui?', 'fas fa-beer'),
+	(14, 'Cuarto', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt commodi deserunt vel. Repellendus explicabo, numquam assumenda corporis consectetur commodi aperiam nostrum modi facilis, nihil fugiat iure nulla sapiente doloremque perferendis?', 'fas fa-caravan'),
+	(18, 'Otros', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores magni id repellendus voluptatum libero quaerat placeat cum! Odio ex amet sapiente quisquam iusto veritatis est natus quibusdam quas a quae, nulla nihil quis dignissimos debitis. Deserunt officia nulla corrupti aliquam.', 'fas fa-water');
 /*!40000 ALTER TABLE `servicios` ENABLE KEYS */;
 
--- Volcando estructura para tabla cabanas.usuarios
 CREATE TABLE IF NOT EXISTS `usuarios` (
   `id_usr` tinyint(3) unsigned NOT NULL AUTO_INCREMENT,
   `user` varchar(50) NOT NULL DEFAULT '',
@@ -80,8 +61,6 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
   KEY `Índice 1` (`id_usr`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
 
--- Volcando datos para la tabla cabanas.usuarios: ~0 rows (aproximadamente)
-DELETE FROM `usuarios`;
 /*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
 INSERT INTO `usuarios` (`id_usr`, `user`, `password`, `isAdmin`) VALUES
 	(1, 'admin', '81dc9bdb52d04dc20036dbd8313ed055', 'S');
